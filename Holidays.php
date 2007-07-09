e7055ec08b50b7b234e07d20fffacb93aa755a3f
Ken Guest <kguest@php.net>
2007-07-09 23:57:56 +0000

    fix most, if not all, phpcs highlighted issues
    git-svn-id: http://svn.php.net/repository/pear/packages/Date_Holidays/trunk@239379 c90b9560-bf6c-de11-be94-00142212c4b1

--- Holidays.php
+++ Holidays.php
@@ -78,44 +78,51 @@ define('DATE_HOLIDAYS_ERROR_MISSING_FILTER_DIR', 4);
  */
 class Date_Holidays
 {
-  /**
-    * Constructor
-    *
-    * Use the Date_Holidays::factory() method to construct an object of a certain driver
-    *
-    * @access   protected
-    */
+    /**
+     * Constructor
+     *
+     * Use the Date_Holidays::factory() method to construct an object of 
+     * a certain driver
+     *
+     * @access   protected
+     */
     function Date_Holidays()
     {
     }
 
-   /**
-    * Factory method that creates a driver-object
-    *
-    * @static
-    * @access   public
-    * @param    string  $driverId   driver-name
-    * @param    string  $year       year
-    * @param    string  $locale     locale name
-    * @return   object  Date_Holidays driver-object on success, otherwise a PEAR_Error object
-    * @throws   object PEAR_Error
-    */
+    /**
+     * Factory method that creates a driver-object
+     *
+     * @param string  $driverId driver-name
+     * @param string  $year     year
+     * @param string  $locale   locale name
+     * @param boolean $external external driver
+     *
+     * @static
+     * @access   public
+     * @return   object  Date_Holidays driver-object on success, 
+     *                   otherwise a PEAR_Error object
+     * @throws   object PEAR_Error
+     */
     function factory($driverId, $year = null, $locale = null, $external = false)
     {
         if (! isset($GLOBALS['_DATE_HOLIDAYS']['DIE_ON_MISSING_LOCALE'])) {
             Date_Holidays::staticSetProperty('DIE_ON_MISSING_LOCALE', true);
         }
 
-        $driverClass        = 'Date_Holidays_Driver_' . $driverId;
+        $driverClass = 'Date_Holidays_Driver_' . $driverId;
         if ($external) {
-            $driverClass    = $driverId;
+            $driverClass = $driverId;
         }
 
         if (! class_exists($driverClass)) {
-            $driverFile     = 'Date' . DIRECTORY_SEPARATOR . 'Holidays' . DIRECTORY_SEPARATOR .
-                    'Driver' . DIRECTORY_SEPARATOR . $driverId . '.php';
+            $driverFile = 'Date' . DIRECTORY_SEPARATOR . 'Holidays' . 
+                          DIRECTORY_SEPARATOR . 'Driver' . 
+                          DIRECTORY_SEPARATOR . $driverId . '.php';
             if ($external) {
-                $driverFile = str_replace('_', DIRECTORY_SEPARATOR, $driverClass) . '.php';
+                $driverFile = str_replace('_', 
+                                          DIRECTORY_SEPARATOR, 
+                                          $driverClass) . '.php';
             }
 
             @include_once $driverFile;
@@ -124,41 +131,46 @@ class Date_Holidays
                     'Couldn\'t find file of the driver-class,  filename: ' . $driverFile);
             }
         }
-        $driver             = new $driverClass;
+        $driver = new $driverClass;
 
         if (is_null($year)) {
-            $year           =   date('Y');
+            $year = date('Y');
         }
         // sets internal var $_year and performs _buildHolidays()
-        $res                =   $driver->setYear($year);
+        $res = $driver->setYear($year);
         if (Date_Holidays::isError($res)) {
             return $res;
         }
 
         if (is_null($locale)) {
-            $locale         =   setlocale(LC_ALL, 0);
+            $locale = setlocale(LC_ALL, 0);
         }
         $driver->setLocale($locale);
         return $driver;
     }
 
-   /**
-    * Factory method that creates a driver-object
-    *
-    * @static
-    * @access   public
-    * @param    string  $isoCode    ISO3166 code identifying the driver
-    * @param    string  $year       year
-    * @param    string  $locale     locale name
-    * @return   object  Date_Holidays driver-object on success, otherwise a PEAR_Error object
-    * @throws   object PEAR_Error
-    */
-    function factoryISO3166($isoCode, $year = null, $locale = null, $external = false)
+    /**
+     * Factory method that creates a driver-object
+     *
+     * @param string  $isoCode  ISO3166 code identifying the driver
+     * @param string  $year     year
+     * @param string  $locale   locale name
+     * @param boolean $external external driver
+     *
+     * @static
+     * @access   public
+     * @return   object  Date_Holidays driver-object on success, otherwise 
+     *                   a PEAR_Error object
+     * @throws   object PEAR_Error
+     */
+    function factoryISO3166($isoCode, 
+                            $year     = null, 
+                            $locale   = null, 
+                            $external = false)
     {
         $driverDir = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Holidays' . DIRECTORY_SEPARATOR . 'Driver';
         if (! is_dir($driverDir)) {
-            return Date_Holidays::raiseError(
-                    DATE_HOLIDAYS_ERROR_DRIVERFILE_NOT_FOUND,
+            return Date_Holidays::raiseError(DATE_HOLIDAYS_ERROR_DRIVERFILE_NOT_FOUND,
                     'Date_Holidays driver directory does not exist');
         }
 
@@ -184,20 +196,21 @@ class Date_Holidays
 
             @include_once $driverFilePath;
             if (! class_exists($driverClass)) {
-                return Date_Holidays::raiseError(
-                        DATE_HOLIDAYS_ERROR_DRIVERFILE_NOT_FOUND,
+                return Date_Holidays::raiseError(DATE_HOLIDAYS_ERROR_DRIVERFILE_NOT_FOUND,
                         'Couldn\'t find file of the driver-class ' . $driverClass
                                 . ',  filename: ' . $driverFilePath);
             }
 
-            $isoCodes = call_user_func(
-                    array(
+            $isoCodes = call_user_func(array(
                             $driverClass,
                             DATE_HOLIDAYS_DRIVER_IDENTIFY_ISO3166_METHOD));
 
             foreach ($isoCodes as $code) {
                 if (strtolower($code) === $isoCode) {
-                    return Date_Holidays::factory($driverId, $year, $locale, $external);
+                    return Date_Holidays::factory($driverId, 
+                                                  $year, 
+                                                  $locale, 
+                                                  $external);
                 }
             }
         }
@@ -211,14 +224,15 @@ class Date_Holidays
 
     }
 
-   /**
-    * Returns a list of the installed drivers
-    *
-    * @access public
-    * @static
-    * @param  string      directory, where the drivers are installed
-    * @return array
-    */
+    /**
+     * Returns a list of the installed drivers
+     *
+     * @param string $directory where the drivers are installed
+     *
+     * @access public
+     * @static
+     * @return array
+     */
     function getInstalledDrivers($directory = null)
     {
         $drivers = array();
@@ -226,19 +240,21 @@ class Date_Holidays
             $directory = dirname(__FILE__) . '/Holidays/Driver';
         }
         if (!file_exists($directory) || !is_dir($directory)) {
-        	return PEAR::raiseError(DATE_HOLIDAYS_ERROR_MISSING_DRIVER_DIR, 'The driver directory "'.$directory.'" does not exist');
+            $msg = 'The driver directory "'.$directory.'" does not exist';
+            return PEAR::raiseError(DATE_HOLIDAYS_ERROR_MISSING_DRIVER_DIR, $msg);
         }
         return Date_Holidays::_getModulesFromDir($directory);
     }
 
-   /**
-    * Returns a list of the installed filters
-    *
-    * @access public
-    * @static
-    * @param  string      directory, where the filters are installed
-    * @return array
-    */
+    /**
+     * Returns a list of the installed filters
+     *
+     * @param string $directory where the filters are installed
+     *
+     * @access public
+     * @static
+     * @return array
+     */
     function getInstalledFilters($directory = null)
     {
         $filters = array();
@@ -246,132 +262,150 @@ class Date_Holidays
             $directory = dirname(__FILE__) . '/Holidays/Filter';
         }
         if (!file_exists($directory) || !is_dir($directory)) {
-        	return PEAR::raiseError(DATE_HOLIDAYS_ERROR_MISSING_FILTER_DIR, 'The filter directory "'.$directory.'" does not exist');
+            $msg = 'The filter directory "'.$directory.'" does not exist';
+            return PEAR::raiseError(DATE_HOLIDAYS_ERROR_MISSING_FILTER_DIR, $msg);
         }
         return Date_Holidays::_getModulesFromDir($directory);
     }
 
-   /**
-    * Fetch all modules from a directory and its subdirectories
-    *
-    * @static
-    * @access protected
-    * @param  string        directory
-    * @param  string        prefix for the class names, will be used in recursive calls
-    */
-    function _getModulesFromDir($dir, $prefix = '')
+    /**
+     * Fetch all modules from a directory and its subdirectories
+     *
+     * @param string $directory specified directory
+     * @param string $prefix    for the class names, will be used in recursive calls
+     *
+     * @static
+     * @access protected
+     * @return array modules
+     */
+    function _getModulesFromDir($directory, $prefix = '')
     {
         $modules = array();
-        $d = dir($dir);
+        $d = dir($directory);
         while (false !== $moduleFile = $d->read()) {
-            if ($moduleFile === '.' || $moduleFile === '..' || $moduleFile === 'CVS') {
-            	continue;
+            if ($moduleFile === '.' || 
+                $moduleFile === '..' || 
+                $moduleFile === 'CVS') {
+                continue;
             }
-            if (is_dir($dir.'/'.$moduleFile)) {
-            	$modules = array_merge($modules, Date_Holidays::_getModulesFromDir($dir.'/'.$moduleFile, $prefix.$moduleFile.'_'));
-            	continue;
+            if (is_dir($directory.'/'.$moduleFile)) {
+                $modules = array_merge($modules, 
+                        Date_Holidays::_getModulesFromDir($directory.'/'.$moduleFile,
+                                                          $prefix.$moduleFile.'_'));
+                continue;
             }
             $matches = array();
             if (preg_match('/(.*)\.php$/', $moduleFile, $matches)) {
-            	array_push($modules, array('id' => $prefix.$matches[1], 'title' => $prefix.$matches[1]));
+                array_push($modules, array('id'    => $prefix.$matches[1], 
+                                           'title' => $prefix.$matches[1]));
             }
         }
         return $modules;
     }
 
-   /**
-    * Returns the error-stack
-    *
-    * @static
-    * @access   public
-    * @return   object PEAR_ErrorStack  error-stack
-    */
+    /**
+     * Returns the error-stack
+     *
+     * @static
+     * @access   public
+     * @return   object PEAR_ErrorStack  error-stack
+     */
     function &getErrorStack()
     {
         return PEAR_ErrorStack::singleton('Date_Holidays', false, false, true);
     }
 
-   /**
-    * Pushes a new error on the error-stack and returns a PEAR_Error object
-    *
-    * @static
-    * @access   public
-    * @param    int     $code   error-code
-    * @param    string  $msg    error-message
-    * @return   object PEAR_Error
-    */
+    /**
+     * Pushes a new error on the error-stack and returns a PEAR_Error object
+     *
+     * @param int    $code error-code
+     * @param string $msg  error-message
+     *
+     * @static
+     * @access   public
+     * @return   object PEAR_Error
+     */
     function raiseError($code, $msg = null)
     {
         $errorStack = &Date_Holidays::getErrorStack();
-        return $errorStack->push($code, 'error', array(), $msg, false, debug_backtrace());
+        return $errorStack->push($code, 
+                                 'error', 
+                                 array(), 
+                                 $msg, 
+                                 false, 
+                                 debug_backtrace());
     }
 
-   /**
-    * Checks a variable to determine whether it represnts an error object or not
-    *
-    * @static
-    * @access   public
-    * @param    mixed   $data   variable to test
-    * @param    int     $code   if $data is an PEAR_Error object, return true
-    *                           only if $code is a string and
-    *                           $obj->getMessage() == $code or
-    *                           $code is an integer and $obj->getCode() == $code
-    * @return   boolean true if $subject is an error object
-    */
+    /**
+     * Checks a variable to determine whether it represnts an error object or not
+     *
+     * @static
+     * @access   public
+     * @param mixed $data variable to test
+     * @param int   $code if $data is an PEAR_Error object, return true
+     *                    only if $code is a string and
+     *                    $obj->getMessage() == $code or
+     *                    $code is an integer and $obj->getCode() == $code
+     * @return   boolean true if $subject is an error object
+     */
     function isError($data, $code = null)
     {
         $errorClass = get_class($data);
         switch (strtolower($errorClass)) {
-            case 'pear_error':
-                return PEAR::isError($data, $code);
-            case 'pear_errorstack':
-                return $data->hasErrors();
+        case 'pear_error':
+            return PEAR::isError($data, $code);
+        case 'pear_errorstack':
+            return $data->hasErrors();
         }
         return false;
     }
 
-   /**
-    * Checks whether errors occured
-    *
-    * @static
-    * @access   public
-    * @return   boolean true if errors occurred
-    */
+    /**
+     * Checks whether errors occured
+     *
+     * @static
+     * @access   public
+     * @return   boolean true if errors occurred
+     */
     function errorsOccurred()
     {
         $errorStack = &Date_Holidays::getErrorStack();
         return $errorStack->hasErrors();
     }
 
-   /**
-    * Returns the errors the error-stack contains
-    *
-    * @static
-    * @access   public
-    * @param    boolean $purge  true if the stall shall be purged
-    * @return   array   errors
-    */
+    /**
+     * Returns the errors the error-stack contains
+     *
+     * @param boolean $purge true if the stall shall be purged
+     *
+     * @static
+     * @access   public
+     * @return   array   errors
+     */
     function getErrors($purge = false)
     {
         $errorStack = &Date_Holidays::getErrorStack();
         return $errorStack->getErrors($purge);
     }
 
-   /**
-    * Set a property for the Date_Holidays drivers
-    *
-    * Available properties:
-    * <pre>
-    * DIE_ON_MISSING_LOCALE = boolean
-    *   false: if no localized holiday-title is found an error will be returned
-    *   true: if no localized holiday-title is found the default translation (English) will be used
-    * </pre>
-    *
-    * @static
-    * @access   public
-    * @param    string  $prop   property
-    * @param    string  $value  property-value
-    */
+    /**
+     * Set a property for the Date_Holidays drivers
+     *
+     * Available properties:
+     * <pre>
+     * DIE_ON_MISSING_LOCALE = boolean
+     *   false: if no localized holiday-title is found an error will be returned
+     *   true: if no localized holiday-title is found then the default 
+     *         translation (English) will be used
+     * </pre>
+     *
+     * @param string $prop  property
+     * @param string $value property-value
+     *
+     * @access   public
+     * @static
+     * @return   void
+     */
     function staticSetProperty($prop, $value)
     {
         if (! isset($GLOBALS['_DATE_HOLIDAYS'])) {
@@ -379,22 +413,23 @@ class Date_Holidays
         }
 
         switch ($prop) {
-            case 'DIE_ON_MISSING_LOCALE':
-                if (is_bool($value)) {
-                    $GLOBALS['_DATE_HOLIDAYS'][$prop] = $value;
-                }
-                break;
+        case 'DIE_ON_MISSING_LOCALE':
+            if (is_bool($value)) {
+                $GLOBALS['_DATE_HOLIDAYS'][$prop] = $value;
+            }
+            break;
         }
     }
 
-   /**
-    * Returns an internal property value
-    *
-    * @static
-    * @access   public
-    * @param    string  $prop   property-name
-    * @return   mixed   property value on success, otherwise null
-    */
+    /**
+     * Returns an internal property value
+     *
+     * @param string $prop property-name
+     *
+     * @static
+     * @access   public
+     * @return   mixed   property value on success, otherwise null
+     */
     function staticGetProperty($prop)
     {
         if (! isset($GLOBALS['_DATE_HOLIDAYS'])) {
@@ -402,10 +437,10 @@ class Date_Holidays
         }
 
         switch ($prop) {
-            case 'DIE_ON_MISSING_LOCALE':
-                if (isset($GLOBALS['_DATE_HOLIDAYS'][$prop])) {
-                    return $GLOBALS['_DATE_HOLIDAYS'][$prop];
-                }
+        case 'DIE_ON_MISSING_LOCALE':
+            if (isset($GLOBALS['_DATE_HOLIDAYS'][$prop])) {
+                return $GLOBALS['_DATE_HOLIDAYS'][$prop];
+            }
         }
 
         return null;
