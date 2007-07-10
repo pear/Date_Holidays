d2eaf4341028da57a473dfc994265bb9f5b4f520
Ken Guest <kguest@php.net>
2007-07-10 23:09:29 +0000

    one less phpcs error...
    git-svn-id: http://svn.php.net/repository/pear/packages/Date_Holidays/trunk@239456 c90b9560-bf6c-de11-be94-00142212c4b1

--- Holidays/Filter/Composite.php
+++ Holidays/Filter/Composite.php
@@ -33,8 +33,8 @@ define('DATE_HOLIDAYS_FILTER_NOT_FOUND', 200);
  * @package  Date_Holidays
  * @subpackage  Filter
  * @author   Carsten Lucke <luckec@tool-garage.de>
- * @version  $Id$
  * @license  http://www.php.net/license/3_01.txt PHP License 3.0.1
+ * @version  $Id$
  * @link     http://pear.php.net/package/Date_Holidays
  */
 class Date_Holidays_Filter_Composite extends Date_Holidays_Filter
