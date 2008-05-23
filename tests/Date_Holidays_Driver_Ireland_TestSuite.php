0a87daae599135e8d30604bd623ada0221657079
Ken Guest <kguest@php.net>
2008-05-23 21:37:11 +0000

    corrected holiday name (boxingDay v StStephensDay). remove 'die' line.
    git-svn-id: http://svn.php.net/repository/pear/packages/Date_Holidays/trunk@260189 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Date_Holidays_Driver_Ireland_TestSuite.php
+++ tests/Date_Holidays_Driver_Ireland_TestSuite.php
@@ -3,7 +3,7 @@
  * Test class for running unit tests related to the driver for holidays in Austria
  *
  * PHP Versions 4 and 5
- * 
+ *
  * @category Date
  * @package  Date_Holidays
  * @author   Carsten Lucke <luckec@tool-garage.de>
@@ -21,7 +21,7 @@ require_once 'Date/Holidays.php';
 
 /**
  * Test class for running unit tests related to the driver for holidays in Austria
- * 
+ *
  * @uses PHPUnit_Framework_TestCase
  * @category Date
  * @package  Date_Holidays
@@ -37,71 +37,71 @@ class Date_Holidays_Driver_Ireland_TestSuite extends PHPUnit_Framework_TestCase
     var $testDates2007;
 
     /**
-     * setUp 
-     * 
+     * setUp
+     *
      * @access public
      * @return void
      */
     function setUp()
     {
-        
+
         $this->testDates2006 = array(
-            'newYearsDay'           => array('day' => 1, 
+            'newYearsDay'           => array('day' => 1,
                                              'month' => 1,
                                              'year' => 2006),
-            'easterMonday'          => array('day' => 17, 
+            'easterMonday'          => array('day' => 17,
                                              'month' => 4,
                                              'year' => 2006),
-            'mayDayBankHoliday'     => array('day' => 1, 
+            'mayDayBankHoliday'     => array('day' => 1,
                                              'month' => 5,
                                              'year' => 2006),
-            'juneBankHoliday'       => array('day' => 5, 
+            'juneBankHoliday'       => array('day' => 5,
                                              'month' => 6,
                                              'year' => 2006),
-            'augustBankHoliday'     => array('day' => 7, 
+            'augustBankHoliday'     => array('day' => 7,
                                              'month' => 8,
                                              'year' => 2006),
-            'stPatricksDay'         => array('day' => 17, 
+            'stPatricksDay'         => array('day' => 17,
                                              'month' => 3,
                                              'year' => 2006),
-            'christmasDay'               => array('day' => 25, 
+            'christmasDay'               => array('day' => 25,
                                              'month' => 12,
                                              'year' => 2006),
-            'boxingDay'             => array('day' => 26, 
+            'StStephensDay'             => array('day' => 26,
                                              'month' => 12,
                                              'year' => 2006)
         );
         $this->testDates2007 = array(
-            'newYearsDay'           => array('day' => 1, 
+            'newYearsDay'           => array('day' => 1,
                                              'month' => 1,
                                              'year' => 2007),
-            'easterMonday'          => array('day' => 9, 
+            'easterMonday'          => array('day' => 9,
                                              'month' => 4,
                                              'year' => 2007),
-            'mayDayBankHoliday'     => array('day' => 7, 
+            'mayDayBankHoliday'     => array('day' => 7,
                                              'month' => 5,
                                              'year' => 2007),
-            'juneBankHoliday'       => array('day' => 4, 
+            'juneBankHoliday'       => array('day' => 4,
                                              'month' => 6,
                                              'year' => 2007),
-            'augustBankHoliday'     => array('day' => 6, 
+            'augustBankHoliday'     => array('day' => 6,
                                              'month' => 8,
                                              'year' => 2007),
-            'stPatricksDay'         => array('day' => 17, 
+            'stPatricksDay'         => array('day' => 17,
                                              'month' => 3,
                                              'year' => 2007),
-            'christmasDay'               => array('day' => 25, 
+            'christmasDay'               => array('day' => 25,
                                              'month' => 12,
                                              'year' => 2007),
-            'boxingDay'             => array('day' => 26, 
+            'StStephensDay'             => array('day' => 26,
                                              'month' => 12,
                                              'year' => 2007)
         );
     }
 
     /**
-     * test Holidays for 2007 
-     * 
+     * test Holidays for 2007
+     *
      * @access public
      * @return void
      */
@@ -113,7 +113,7 @@ class Date_Holidays_Driver_Ireland_TestSuite extends PHPUnit_Framework_TestCase
             print_r($drv);
             die($drv->getMessage());
         }
-        
+
         foreach ($this->testDates2007 as $name => $dateInfo) {
             $day = $drv->getHoliday($name);
             $this->assertFalse(Date_Holidays::isError($day));
@@ -129,8 +129,8 @@ class Date_Holidays_Driver_Ireland_TestSuite extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * test Holidays for 2006 
-     * 
+     * test Holidays for 2006
+     *
      * @access public
      * @return void
      */
@@ -142,13 +142,11 @@ class Date_Holidays_Driver_Ireland_TestSuite extends PHPUnit_Framework_TestCase
             print_r($drv);
             die($drv->getMessage());
         }
-        
+
         foreach ($this->testDates2006 as $name => $dateInfo) {
+
             $day = $drv->getHoliday($name);
             $this->assertFalse(Date_Holidays::isError($day));
-            if (Date_Holidays::isError($day)) {
-                die($day->getMessage());
-            }
             $this->assertEquals($name, $day->getInternalName());
             $date = $day->getDate();
             $this->assertEquals($dateInfo['day'], $date->getDay(), $name);
