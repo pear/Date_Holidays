086c4678cfa2aeb5aeb3a94c4d3f38937a4b36c8
Ken Guest <kguest@php.net>
2008-08-05 23:47:12 +0000

    Request #13750 Make use of easter_date()
    git-svn-id: http://svn.php.net/repository/pear/packages/Date_Holidays/trunk@264263 c90b9560-bf6c-de11-be94-00142212c4b1

--- Holidays/Driver/Christian.php
+++ Holidays/Driver/Christian.php
@@ -395,6 +395,9 @@ class Date_Holidays_Driver_Christian extends Date_Holidays_Driver
      */
     function calcEaster($year)
     {
+        if (function_exists(easter_days())) {
+            return easter_days($year);
+        }
         // golden number
         $golden  = null;
         $century = null;
