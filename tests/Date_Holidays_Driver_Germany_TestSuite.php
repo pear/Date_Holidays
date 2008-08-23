928718d1285c4e7a2819ca27626737211ed5f43d
Ken Guest <kguest@php.net>
2008-08-23 08:08:44 +0000

    TestSuite changes
    git-svn-id: http://svn.php.net/repository/pear/packages/Date_Holidays/trunk@265321 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Date_Holidays_Driver_Germany_TestSuite.php
+++ tests/Date_Holidays_Driver_Germany_TestSuite.php
@@ -3,7 +3,7 @@
  * Test class for running unit tests related to the driver for holidays in Austria
  *
  * PHP Versions 4 and 5
- * 
+ *
  * @category Date
  * @package  Date_Holidays
  * @author   Carsten Lucke <luckec@tool-garage.de>
@@ -15,8 +15,6 @@ if (!defined("PHPUnit_MAIN_METHOD")) {
     define("PHPUnit_MAIN_METHOD", "Date_HolidaysTest::main");
 }
 
-//make cvs testing work
-chdir(dirname(__FILE__) . '/../');
 require_once 'Date/Holidays.php';
 
 /**
@@ -37,140 +35,140 @@ class Date_Holidays_Driver_Germany_TestSuite extends PHPUnit_Framework_TestCase
     var $testDates2006;
 
     /**
-     * Setup 
-     * 
+     * Setup
+     *
      * @access public
      * @return void
      */
     function setUp()
     {
         $this->testDates2005 = array(
-                'newYearsDay'               => array('day' => 1, 
+                'newYearsDay'               => array('day' => 1,
                                                      'month' => 1,
                                                      'year' => 2005),
-                'valentinesDay'             => array('day' => 14, 
+                'valentinesDay'             => array('day' => 14,
                                                      'month' => 2,
                                                      'year' => 2005),
-                'womenFasnet'               => array('day' => 3, 
+                'womenFasnet'               => array('day' => 3,
                                                      'month' => 2,
                                                      'year' => 2005),
-                'fasnet'                    => array('day' => 8, 
+                'fasnet'                    => array('day' => 8,
                                                      'month' => 2,
                                                      'year' => 2005),
-                'roseMonday'                => array('day' => 7, 
+                'roseMonday'                => array('day' => 7,
                                                      'month' => 2,
                                                      'year' => 2005),
-                'womensDay'                 => array('day' => 8, 
+                'womensDay'                 => array('day' => 8,
                                                      'month' => 3,
                                                      'year' => 2005),
-                'april1st'                  => array('day' => 1, 
+                'april1st'                  => array('day' => 1,
                                                      'month' => 4,
                                                      'year' => 2005),
-                'girlsDay'                  => array('day' => 28, 
+                'girlsDay'                  => array('day' => 28,
                                                      'month' => 4,
                                                      'year' => 2005),
-                'earthDay'                  => array('day' => 22, 
+                'earthDay'                  => array('day' => 22,
                                                      'month' => 4,
                                                      'year' => 2005),
-                'beersDay'                  => array('day' => 23, 
+                'beersDay'                  => array('day' => 23,
                                                      'month' => 4,
                                                      'year' => 2005),
-                'walpurgisNight'            => array('day' => 30, 
+                'walpurgisNight'            => array('day' => 30,
                                                      'month' => 4,
                                                      'year' => 2005),
-                'dayOfWork'                 => array('day' => 1, 
+                'dayOfWork'                 => array('day' => 1,
                                                      'month' => 5,
                                                      'year' => 2005),
-                'laughingDay'               => array('day' => 1, 
+                'laughingDay'               => array('day' => 1,
                                                      'month' => 5,
                                                      'year' => 2005),
-                'europeDay'                 => array('day' => 5, 
+                'europeDay'                 => array('day' => 5,
                                                      'month' => 5,
                                                      'year' => 2005),
-                'mothersDay'                => array('day' => 8, 
+                'mothersDay'                => array('day' => 8,
                                                      'month' => 5,
                                                      'year' => 2005),
-                'endOfWWar2'                => array('day' => 8, 
+                'endOfWWar2'                => array('day' => 8,
                                                      'month' => 5,
                                                      'year' => 2005),
-                'fathersDay'                => array('day' => 5, 
+                'fathersDay'                => array('day' => 5,
                                                      'month' => 5,
                                                      'year' => 2005),
-                'aiDay'                     => array('day' => 28, 
+                'aiDay'                     => array('day' => 28,
                                                      'month' => 5,
                                                      'year' => 2005),
-                'intChildrenDay'            => array('day' => 1, 
+                'intChildrenDay'            => array('day' => 1,
                                                      'month' => 6,
                                                      'year' => 2005),
-                'organDonationDay'          => array('day' => 4, 
+                'organDonationDay'          => array('day' => 4,
                                                      'month' => 6,
                                                      'year' => 2005),
-                'dormouseDay'               => array('day' => 27, 
+                'dormouseDay'               => array('day' => 27,
                                                      'month' => 6,
                                                      'year' => 2005),
-                'christopherStreetDay'      => array('day' => 27, 
+                'christopherStreetDay'      => array('day' => 27,
                                                      'month' => 6,
                                                      'year' => 2005),
-                'hiroshimaCommemorationDay' => array('day' => 6, 
+                'hiroshimaCommemorationDay' => array('day' => 6,
                                                      'month' => 8,
                                                      'year' => 2005),
-                'augsburgPeaceCelebration'  => array('day' => 8, 
+                'augsburgPeaceCelebration'  => array('day' => 8,
                                                      'month' => 8,
                                                      'year' => 2005),
-                'leftHandedDay'             => array('day' => 13, 
+                'leftHandedDay'             => array('day' => 13,
                                                      'month' => 8,
                                                      'year' => 2005),
-                'antiWarDay'                => array('day' => 1, 
+                'antiWarDay'                => array('day' => 1,
                                                      'month' => 9,
                                                      'year' => 2005),
-                'germanLanguageDay'         => array('day' => 10, 
+                'germanLanguageDay'         => array('day' => 10,
                                                      'month' => 9,
                                                      'year' => 2005),
-                'diabetesDay'               => array('day' => 14, 
+                'diabetesDay'               => array('day' => 14,
                                                      'month' => 11,
                                                      'year' => 2005),
-                'germanUnificationDay'      => array('day' => 3, 
+                'germanUnificationDay'      => array('day' => 3,
                                                      'month' => 10,
                                                      'year' => 2005),
-                'librariesDay'              => array('day' => 24, 
+                'librariesDay'              => array('day' => 24,
                                                      'month' => 10,
                                                      'year' => 2005),
-                'savingsDay'                => array('day' => 30, 
+                'savingsDay'                => array('day' => 30,
                                                      'month' => 10,
                                                      'year' => 2005),
-                'halloween'                 => array('day' => 31, 
+                'halloween'                 => array('day' => 31,
                                                      'month' => 10,
                                                      'year' => 2005),
-                'stampsDay'                 => array('day' => 30, 
+                'stampsDay'                 => array('day' => 30,
                                                      'month' => 10,
                                                      'year' => 2005),
-                'mensDay'                   => array('day' => 3, 
+                'mensDay'                   => array('day' => 3,
                                                      'month' => 11,
                                                      'year' => 2005),
-                'wallOfBerlin'              => array('day' => 9, 
+                'wallOfBerlin'              => array('day' => 9,
                                                      'month' => 11,
                                                      'year' => 2005),
-                'carnivalBeginning'         => array('day' => 11, 
+                'carnivalBeginning'         => array('day' => 11,
                                                      'month' => 11,
                                                      'year' => 2005),
-                'dayOfMourning'             => array('day' => 13, 
+                'dayOfMourning'             => array('day' => 13,
                                                      'month' => 11,
                                                      'year' => 2005)
                     );
 
         $this->testDates2006 = array(
-                'girlsDay'             => array('day' => 27, 
+                'girlsDay'             => array('day' => 27,
                                                 'month' => 4,
                                                 'year' => 2006),
-                'christopherStreetDay' => array('day' => 27, 
+                'christopherStreetDay' => array('day' => 27,
                                                 'month' => 6,
                                                 'year' => 2006)
                 );
-    } 
+    }
 
     /**
-     * test Holidays for 2005 
-     * 
+     * test Holidays for 2005
+     *
      * @access public
      * @return void
      */
@@ -200,8 +198,8 @@ class Date_Holidays_Driver_Germany_TestSuite extends PHPUnit_Framework_TestCase
 
 
     /**
-     * test Holidays for 2006 
-     * 
+     * test Holidays for 2006
+     *
      * @access public
      * @return void
      */
@@ -230,8 +228,8 @@ class Date_Holidays_Driver_Germany_TestSuite extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * test Holidays for 2005 stamps and savings day 
-     * 
+     * test Holidays for 2005 stamps and savings day
+     *
      * @access public
      * @return void
      */
@@ -243,11 +241,11 @@ class Date_Holidays_Driver_Germany_TestSuite extends PHPUnit_Framework_TestCase
             die($drv->getMessage());
         }
         $holidays = $drv->getHolidayForDate('2005-10-30', null, true);
-        $this->assertEquals('savingsDay', 
-                            $holidays[0]->getInternalName(), 
+        $this->assertEquals('savingsDay',
+                            $holidays[0]->getInternalName(),
                             'should be savingsDay');
-        $this->assertEquals('stampsDay', 
-                            $holidays[1]->getInternalName(), 
+        $this->assertEquals('stampsDay',
+                            $holidays[1]->getInternalName(),
                             'should be stampsDay');
     }
 
