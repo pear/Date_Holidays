3e6f5998c856e72550480a308a600bf976cdec6e
Ken Guest <kguest@php.net>
2011-02-28 23:03:27 +0000

    correct license details
    git-svn-id: http://svn.php.net/repository/pear/packages/Date_Holidays/trunk@308782 c90b9560-bf6c-de11-be94-00142212c4b1

--- Date/Holidays/Driver/AustraliaWA.php
+++ Date/Holidays/Driver/AustraliaWA.php
@@ -6,20 +6,10 @@
  *
  * PHP Version 4
  *
- * Copyright (c) 1997-2008 The PHP Group
- *
- * This source file is subject to version 3.0 of the PHP license,
- * that is bundled with this package in the file LICENSE, and is
- * available at through the world-wide-web at
- * http://www.php.net/license/3_01.txt.
- * If you did not receive a copy of the PHP license and are unable to
- * obtain it through the world-wide-web, please send a note to
- * license@php.net so we can mail you a copy immediately.
- *
  * @category Date
  * @package  Date_Holidays
  * @author   Sam Wilson <sam@archives.org.au>
- * @license  http://www.php.net/license/3_01.txt PHP License 3.0.1
+ * @license  BSD http://www.opensource.org/licenses/bsd-license.php
  * @link     http://pear.php.net/package/Date_Holidays
  */
 
@@ -33,7 +23,7 @@ require_once 'Date/Calc.php';
  * @package    Date_Holidays
  * @subpackage Driver
  * @author     Sam Wilson <sam@archives.org.au>
- * @license    http://www.php.net/license/3_01.txt PHP License 3.0.1
+ * @license    BSD http://www.opensource.org/licenses/bsd-license.php
  * @link       http://pear.php.net/package/Date_Holidays
  */
 class Date_Holidays_Driver_AustraliaWA extends Date_Holidays_Driver
