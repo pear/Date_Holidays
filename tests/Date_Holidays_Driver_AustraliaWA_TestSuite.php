ae11fadccb7fdd0a06f086c642ee5375a86269f8
Ken Guest <kguest@php.net>
2011-09-27 20:32:09 +0000

    remove leading zeroes for date values (don't want them misinterpreted as octal values)
    git-svn-id: http://svn.php.net/repository/pear/packages/Date_Holidays/trunk@317412 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Date_Holidays_Driver_AustraliaWA_TestSuite.php
+++ tests/Date_Holidays_Driver_AustraliaWA_TestSuite.php
@@ -37,36 +37,36 @@ class Date_Holidays_Driver_AustraliaWA_TestSuite extends PHPUnit_Framework_TestC
         $holidays = array();
 
         //2009
-        $holidays[] = array("labourDay", 2009, array('day' => 02, 'month' => 05, 'year' => 2009));
-        $holidays[] = array("foundationDay", 2009, array('day' => 01, 'month' => 06, 'year' => 2009));
-        $holidays[] = array("queensBirthday", 2009, array('day' => 28, 'month' => 09, 'year' => 2009));//Non-metropolitan local authorities may celebrate the Queen's Birthday Public Holiday on and alternative date
+        $holidays[] = array("labourDay", 2009, array('day' => 2, 'month' => 5, 'year' => 2009));
+        $holidays[] = array("foundationDay", 2009, array('day' => 1, 'month' => 6, 'year' => 2009));
+        $holidays[] = array("queensBirthday", 2009, array('day' => 28, 'month' => 9, 'year' => 2009));//Non-metropolitan local authorities may celebrate the Queen's Birthday Public Holiday on and alternative date
         $holidays[] = array("boxingDay", 2009, array('day' => 26, 'month' => 12, 'year' => 2009));
 
         // 2010
-        $holidays[] = array("labourDay", 2010, array('day' => 01, 'month' => 05, 'year' => 2010));
-        $holidays[] = array("foundationDay", 2010, array('day' => 07, 'month' => 06, 'year' => 2010));
-        $holidays[] = array("queensBirthday", 2010, array('day' => 27, 'month' => 09, 'year' => 2010));//Non-metropolitan local authorities may celebrate the Queen's Birthday Public Holiday on and alternative date
+        $holidays[] = array("labourDay", 2010, array('day' => 1, 'month' => 5, 'year' => 2010));
+        $holidays[] = array("foundationDay", 2010, array('day' => 7, 'month' => 6, 'year' => 2010));
+        $holidays[] = array("queensBirthday", 2010, array('day' => 27, 'month' => 9, 'year' => 2010));//Non-metropolitan local authorities may celebrate the Queen's Birthday Public Holiday on and alternative date
         $holidays[] = array("boxingDay", 2010, array('day' => 26, 'month' => 12, 'year' => 2010));
 
 
         // 2011
-        $holidays[] = array("labourDay", 2011, array('day' => 07, 'month' => 05, 'year' => 2011));
-        $holidays[] = array("foundationDay", 2011, array('day' => 06, 'month' => 06, 'year' => 2011));
+        $holidays[] = array("labourDay", 2011, array('day' => 7, 'month' => 5, 'year' => 2011));
+        $holidays[] = array("foundationDay", 2011, array('day' => 6, 'month' => 6, 'year' => 2011));
 
         //The date of the Queen's Birthday public holiday for 2011 has been changed from Monday 3 October to Friday 28 October 2011 - 1/03/2011
         $holidays[] = array("queensBirthday", 2011, array('day' => 28, 'month' => 10, 'year' => 2011));//Non-metropolitan local authorities may celebrate the Queen's Birthday Public Holiday on and alternative date
         $holidays[] = array("boxingDay", 2011, array('day' => 27, 'month' => 12, 'year' => 2011));
 
         // 2012
-        $holidays[] = array("labourDay", 2012, array('day' => 05, 'month' => 05, 'year' => 2012));
-        $holidays[] = array("foundationDay", 2012, array('day' => 04, 'month' => 06, 'year' => 2012));
-        $holidays[] = array("queensBirthday", 2012, array('day' => 01, 'month' => 10, 'year' => 2012));//Non-metropolitan local authorities may celebrate the Queen's Birthday Public Holiday on and alternative date
+        $holidays[] = array("labourDay", 2012, array('day' => 5, 'month' => 5, 'year' => 2012));
+        $holidays[] = array("foundationDay", 2012, array('day' => 4, 'month' => 6, 'year' => 2012));
+        $holidays[] = array("queensBirthday", 2012, array('day' => 1, 'month' => 10, 'year' => 2012));//Non-metropolitan local authorities may celebrate the Queen's Birthday Public Holiday on and alternative date
         $holidays[] = array("boxingDay", 2012, array('day' => 26, 'month' => 12, 'year' => 2012));
 
         // 2013
-        $holidays[] = array("labourDay", 2013, array('day' => 04, 'month' => 05, 'year' => 2013));
-        $holidays[] = array("foundationDay", 2013, array('day' => 03, 'month' => 06, 'year' => 2013));
-        $holidays[] = array("queensBirthday", 2013, array('day' => 30, 'month' => 09, 'year' => 2013));//Non-metropolitan local authorities may celebrate the Queen's Birthday Public Holiday on and alternative date
+        $holidays[] = array("labourDay", 2013, array('day' => 4, 'month' => 5, 'year' => 2013));
+        $holidays[] = array("foundationDay", 2013, array('day' => 3, 'month' => 6, 'year' => 2013));
+        $holidays[] = array("queensBirthday", 2013, array('day' => 30, 'month' => 9, 'year' => 2013));//Non-metropolitan local authorities may celebrate the Queen's Birthday Public Holiday on and alternative date
         $holidays[] = array("boxingDay", 2013, array('day' => 26, 'month' => 12, 'year' => 2013));
 
 
