bd90db08118bbfeb8d87fb021d1f74ea960f067d
Daniel O'Connor <clockwerx@php.net>
2012-01-02 23:09:54 +0000

    Implement missing holidays
    git-svn-id: http://svn.php.net/repository/pear/packages/Date_Holidays/trunk@321699 c90b9560-bf6c-de11-be94-00142212c4b1

--- Date/Holidays/Driver/AustraliaNSW.php
+++ Date/Holidays/Driver/AustraliaNSW.php
@@ -60,12 +60,66 @@ class Date_Holidays_Driver_AustraliaNSW extends Date_Holidays_Driver
          */
         $labourDay = Date_Calc::nWeekdayOfMonth(1, 1, 10, $this->_year);
         $this->_addHoliday('labourDay', $labourDay, "Labour Day");
+        $this->_addTranslationForHoliday('labourDay', 'en_EN', "Labour Day");
 
         /*
          * See http://en.wikipedia.org/wiki/Queen%27s_Official_Birthday#Australia
          */
-        $foundationDay = Date_Calc::nWeekdayOfMonth(2, 1, 6, $this->_year);
-        $this->_addHoliday('queensBirthday', $foundationDay, "Queen's Birthday");
+        $queensBirthday = Date_Calc::nWeekdayOfMonth(2, 1, 6, $this->_year);
+        $this->_addHoliday('queensBirthday', $queensBirthday, "Queen's Birthday");
+        $this->_addTranslationForHoliday('queensBirthday', 'en_EN', "Queen's Birthday");
+
+        /**
+         * Christmas and Boxing Day
+         */
+        $christmasDay = new Date($this->_year . '-12-25');
+        if ($christmasDay->getDayOfWeek() == 6) {
+            // 25 December - if that date falls on a Saturday the public holiday transfers to the following Monday.
+            $this->_addHoliday('christmasDay',
+                               $this->_year . '-12-27',
+                               'Substitute Bank Holiday in lieu of Christmas Day');
+
+        } else if ($christmasDay->getDayOfWeek() == 0) {
+            // If that date falls on a Sunday that day and the following Monday will be public holidays.
+            $this->_addHoliday('christmasDay',
+                               $this->_year . '-12-26',
+                               'Substitute Bank Holiday in lieu of Christmas Day');
+        } else {
+            $this->_addHoliday('christmasDay', $christmasDay, 'Christmas Day');
+        }
+
+        $boxingDay = new Date($this->_year . '-12-26');
+        if ($boxingDay->getDayOfWeek() == 6) {
+            //26 December - if that date falls on a Saturday the public holiday transfers to the following Monday.
+            $this->_addHoliday('boxingDay',
+                               $this->_year . '-12-28',
+                               'Substitute Bank Holiday in lieu of Boxing Day');
+        } else if ($boxingDay->getDayOfWeek() == 0) {
+            // If that date falls on a Sunday that day and the following Tuesday will be public holidays.
+            $this->_addHoliday('boxingDay',
+                               $this->_year . '-12-28',
+                               'Substitute Bank Holiday in lieu of Boxing Day');
+        } else if ($boxingDay->getDayOfWeek() == 1) {
+            // If that date falls on a Monday that day and the following Tuesday will be public holidays.
+            $this->_addHoliday('boxingDay',
+                               $this->_year . '-12-26',
+                               'Substitute Bank Holiday in lieu of Boxing Day');
+            $this->_addHoliday('boxingDay',
+                               $this->_year . '-12-27',
+                               'Substitute Bank Holiday in lieu of Boxing Day');
+        } else {
+            $this->_addHoliday('boxingDay', $this->_year . '-12-26', 'Boxing Day');
+        }
+
+        $this->_addTranslationForHoliday('christmasDay', 'en_EN', 'Christmas Day');
+        $this->_addTranslationForHoliday('boxingDay', 'en_EN', 'Boxing Day');
+
+
+        // http://www.legislation.nsw.gov.au/maintop/view/inforce/act+49+2008+cd+0+N
+        //  the first Monday in August (Bank Holiday).
+        $bankHoliday = Date_Calc::nWeekdayOfMonth(1, 1, 8, $this->_year);
+        $this->_addHoliday('bankHoliday', $bankHoliday, 'Bank Holiday');
+        $this->_addTranslationForHoliday('bankHoliday', 'en_EN', 'Bank Holiday');
 
     } // _buildHolidays()
 
