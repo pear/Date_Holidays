ae6b3a8d3bf32e6ecb9e6713a92676aee5c5cfa4
Daniel O'Connor <clockwerx@php.net>
2012-01-04 14:16:29 +0000

    Fix Australia WA  unit tests
    git-svn-id: http://svn.php.net/repository/pear/packages/Date_Holidays/trunk@321765 c90b9560-bf6c-de11-be94-00142212c4b1

--- Date/Holidays/Driver/AustraliaWA.php
+++ Date/Holidays/Driver/AustraliaWA.php
@@ -61,6 +61,7 @@ class Date_Holidays_Driver_AustraliaWA extends Date_Holidays_Driver
          */
         $labourDay = Date_Calc::nWeekdayOfMonth(1, 1, 3, $this->_year);
         $this->_addHoliday('labourDay', $labourDay, "Labour Day");
+        $this->_addTranslationForHoliday('labourDay', 'en_EN', "Labour Day");
 
         /*
          * Foundation Day (Queen's Birthday in other states).
@@ -68,6 +69,7 @@ class Date_Holidays_Driver_AustraliaWA extends Date_Holidays_Driver
          */
         $foundationDay = Date_Calc::nWeekdayOfMonth(1, 1, 6, $this->_year);
         $this->_addHoliday('foundationDay', $foundationDay, "Foundation Day");
+        $this->_addTranslationForHoliday('foundationDay', 'en_EN', "Foundation Day");
 
         /*
          * The Queen's Birthday.  There is no firm rule to determine this date before
@@ -96,6 +98,53 @@ class Date_Holidays_Driver_AustraliaWA extends Date_Holidays_Driver
             }
         }
         $this->_addHoliday('queensBirthday', $queensBirthday, "Queen's Birthday");
+        $this->_addTranslationForHoliday('queensBirthday', 'en_EN', "Queen's Birthday");
+
+
+        /**
+         * Christmas and Boxing Day
+         */
+        $christmasDay = new Date($this->_year . '-12-25');
+        if ($christmasDay->getDayOfWeek() == 6) {
+            // 25 December - if that date falls on a Saturday the public holiday transfers to the following Monday.
+            $this->_addHoliday('christmasDay',
+                               $this->_year . '-12-27',
+                               'Substitute Bank Holiday in lieu of Christmas Day');
+
+        } else if ($christmasDay->getDayOfWeek() == 0) {
+            // If that date falls on a Sunday that day and the following Monday will be public holidays.
+            $this->_addHoliday('christmasDay',
+                               $this->_year . '-12-26',
+                               'Substitute Bank Holiday in lieu of Christmas Day');
+        } else {
+            $this->_addHoliday('christmasDay', $christmasDay, 'Christmas Day');
+        }
+
+        $boxingDay = new Date($this->_year . '-12-26');
+        if ($boxingDay->getDayOfWeek() == 6) {
+            //26 December - if that date falls on a Saturday the public holiday transfers to the following Monday.
+            $this->_addHoliday('boxingDay',
+                               $this->_year . '-12-28',
+                               'Substitute Bank Holiday in lieu of Boxing Day');
+        } else if ($boxingDay->getDayOfWeek() == 0) {
+            // If that date falls on a Sunday that day and the following Tuesday will be public holidays.
+            $this->_addHoliday('boxingDay',
+                               $this->_year . '-12-28',
+                               'Substitute Bank Holiday in lieu of Boxing Day');
+        } else if ($boxingDay->getDayOfWeek() == 1) {
+            // If that date falls on a Monday that day and the following Tuesday will be public holidays.
+            $this->_addHoliday('boxingDay',
+                               $this->_year . '-12-26',
+                               'Substitute Bank Holiday in lieu of Boxing Day');
+            $this->_addHoliday('boxingDay',
+                               $this->_year . '-12-27',
+                               'Substitute Bank Holiday in lieu of Boxing Day');
+        } else {
+            $this->_addHoliday('boxingDay', $this->_year . '-12-26', 'Boxing Day');
+        }
+
+        $this->_addTranslationForHoliday('christmasDay', 'en_EN', 'Christmas Day');
+        $this->_addTranslationForHoliday('boxingDay', 'en_EN', 'Boxing Day');
 
     } // _buildHolidays()
 
