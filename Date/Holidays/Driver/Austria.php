93928c7d7858fa7828855ece34273f92cf737c94
Daniel O'Connor <clockwerx@php.net>
2012-04-03 00:52:10 +0000

    Added english translations
    git-svn-id: http://svn.php.net/repository/pear/packages/Date_Holidays/trunk@324726 c90b9560-bf6c-de11-be94-00142212c4b1

--- Date/Holidays/Driver/Austria.php
+++ Date/Holidays/Driver/Austria.php
@@ -75,6 +75,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
          * New Year's Day
          */
         $this->_addHoliday('newYearsDay', $this->_year . '-01-01', 'Neujahr');
+        $this->_addTranslationForHoliday('newYearsDay', 'en_EN', 'New Year\'s Day');
 
         /**
          * Epiphanias
@@ -82,6 +83,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('epiphany',
                            $this->_year . '-01-06',
                            'Heilige Drei Könige');
+        $this->_addTranslationForHoliday('epiphany', 'en_EN', 'Epiphany');
 
         /**
          * Valentine´s Day
@@ -89,51 +91,60 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('valentinesDay',
                            $this->_year . '-02-14',
                            'Valentinstag');
+        $this->_addTranslationForHoliday('valentinesDay', 'en_EN', 'Valentines Day');
 
         /**
          * Easter Sunday
          */
         $easterDate = Date_Holidays_Driver_Christian::calcEaster($this->_year);
         $this->_addHoliday('easter', $easterDate, 'Ostersonntag');
+        $this->_addTranslationForHoliday('easter', 'en_EN', 'Easter');
 
         /**
          * Ash Wednesday
          */
         $ashWednesday = $this->_addDays($easterDate, -46);
         $this->_addHoliday('ashWednesday', $ashWednesday, 'Aschermittwoch');
+        $this->_addTranslationForHoliday('ashWednesday', 'en_EN', 'Ash Wednesday');
 
         /**
          * Palm Sunday
          */
         $palmSunday = $this->_addDays($easterDate, -7);
         $this->_addHoliday('palmSunday', $palmSunday, 'Palmsonntag');
+        $this->_addTranslationForHoliday('palmSunday', 'en_EN', 'Palm Sunday');
 
         /**
          * Maundy Thursday
          */
         $maundyThursday = $this->_addDays($easterDate, -3);
         $this->_addHoliday('maundyThursday', $maundyThursday, 'Gründonnerstag');
+        $this->_addTranslationForHoliday('maundyThursday', 'en_EN', 'Maundy Thursday');
 
         /**
          * Good Friday
          */
         $goodFriday = $this->_addDays($easterDate, -2);
         $this->_addHoliday('goodFriday', $goodFriday, 'Karfreitag');
+        $this->_addTranslationForHoliday('goodFriday', 'en_EN', 'Good Friday');
 
         /**
          * Easter Monday
          */
         $this->_addHoliday('easterMonday', $easterDate->getNextDay(), 'Ostermontag');
+        $this->_addTranslationForHoliday('easterMonday', 'en_EN', 'Easter Monday');
 
         /**
          * Day of Work
          */
         $this->_addHoliday('dayOfWork', $this->_year . '-05-01', 'Staatsfeiertag Österreich');
+        $this->_addTranslationForHoliday('easterMonday', 'en_EN', 'Day of Work');
 
         /**
          * Saint Florian
          */
         $this->_addHoliday('saintFlorian', $this->_year . '-05-04', 'St. Florian');
+        $this->_addTranslationForHoliday('saintFlorian', 'en_EN', 'St. Florian');
 
         /**
          * Mothers Day
@@ -142,12 +153,14 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $mothersDay = $mothersDay->getPrevDay();
         $mothersDay = $this->_addDays($mothersDay, 7);
         $this->_addHoliday('mothersDay', $mothersDay, 'Muttertag');
+        $this->_addTranslationForHoliday('mothersDay', 'en_EN', 'Mothers Day');
 
         /**
          * Ascension Day
          */
         $ascensionDate = $this->_addDays($easterDate, 39);
         $this->_addHoliday('ascensionDate', $ascensionDate, 'Christi Himmelfahrt');
+        $this->_addTranslationForHoliday('ascensionDate', 'en_EN', 'Ascension Day');
 
         /**
          * Ascension Day
@@ -164,17 +177,20 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
          */
         $whitsunDate = $this->_addDays($easterDate, 49);
         $this->_addHoliday('whitsun', $whitsunDate, 'Pfingstsonntag');
+        $this->_addTranslationForHoliday('whitsun', 'en_EN', 'Whitsun');
 
         /**
          * Whit Monday
          */
         $this->_addHoliday('whitMonday', $whitsunDate->getNextDay(), 'Pfingstmontag');
+        $this->_addTranslationForHoliday('whitMonday', 'en_EN', 'Whit Monday');
 
         /**
          * Corpus Christi
          */
         $corpusChristi = $this->_addDays($easterDate, 60);
         $this->_addHoliday('corpusChristi', $corpusChristi, 'Fronleichnam');
+        $this->_addTranslationForHoliday('corpusChristi', 'en_EN', 'Corpus Christi');
 
         /**
          * Fathers Day
@@ -185,6 +201,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('fathersDay',
                            $fathersDay,
                            'Vatertag');
+        $this->_addTranslationForHoliday('fathersDay', 'en_EN', 'Fathers Day');
 
         /**
          * Ascension of Maria
@@ -192,6 +209,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('mariaAscension',
                            $this->_year . '-08-15',
                            'Maria Himmelfahrt');
+        $this->_addTranslationForHoliday('mariaAscension', 'en_EN', 'Ascension of Maria');
 
         /**
          * Österreichischer Nationalfeiertag
@@ -199,6 +217,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('nationalDayAustria',
                            $this->_year . '-10-26',
                            'Österreichischer Nationalfeiertag');
+        $this->_addTranslationForHoliday('nationalDayAustria', 'en_EN', 'Österreichischer Nationalfeiertag');
 
         /**
          * All Saints' Day
@@ -206,6 +225,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('allSaintsDay',
                            $this->_year . '-11-01',
                            'Allerheiligen');
+        $this->_addTranslationForHoliday('allSaintsDay', 'en_EN', 'All Saints Day');
 
         /**
          *All Souls´ Day
@@ -213,6 +233,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('allSoulsDay',
                            $this->_year . '-11-02',
                            'Allerseelen');
+        $this->_addTranslationForHoliday('allSoulsDay', 'en_EN', 'All Souls Day');
 
         /**
          * Santa Claus
@@ -220,6 +241,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('santasDay',
                            $this->_year . '-12-06',
                            'St. Nikolaus');
+        $this->_addTranslationForHoliday('santasDay', 'en_EN', 'St. Nikolaus');
 
         /**
          * Immaculate Conception
@@ -227,6 +249,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('immaculateConceptionDay',
                            $this->_year . '-12-08',
                            'Maria Empfängnis');
+        $this->_addTranslationForHoliday('immaculateConceptionDay', 'en_EN', 'Immaculate Conception Day');
 
         /**
          * Sunday in commemoration of the dead (sundayIcotd)
@@ -236,6 +259,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('sundayIcotd',
                            $sundayIcotd,
                            'Totensonntag');
+        $this->_addTranslationForHoliday('sundayIcotd', 'en_EN', 'Sunday in commemoration of the dead');
 
         /**
          * 1. Advent
@@ -245,6 +269,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('firstAdvent',
                            $firstAdv,
                            '1. Advent');
+        $this->_addTranslationForHoliday('firstAdvent', 'en_EN', '1. Advent');
 
         /**
          * 2. Advent
@@ -253,6 +278,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('secondAdvent',
                            $secondAdv,
                            '2. Advent');
+        $this->_addTranslationForHoliday('secondAdvent', 'en_EN', '2. Advent');
 
         /**
          * 3. Advent
@@ -261,6 +287,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('thirdAdvent',
                            $thirdAdv,
                            '3. Advent');
+        $this->_addTranslationForHoliday('thirdAdvent', 'en_EN', '3. Advent');
 
         /**
          * 4. Advent
@@ -269,6 +296,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('fourthAdvent',
                            $fourthAdv,
                            '4. Advent');
+        $this->_addTranslationForHoliday('fourthAdvent', 'en_EN', '4. Advent');
 
         /**
          * Christmas Eve
@@ -276,6 +304,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('christmasEve',
                            $this->_year . '-12-24',
                            'Heiliger Abend');
+        $this->_addTranslationForHoliday('christmasEve', 'en_EN', 'Christmas Eve');
 
         /**
          * Christmas day
@@ -283,6 +312,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('christmasDay',
                            $this->_year . '-12-25',
                            'Christtag');
+        $this->_addTranslationForHoliday('christmasDay', 'en_EN', 'Christmas Day');
 
         /**
          * Boxing day
@@ -290,6 +320,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('boxingDay',
                            $this->_year . '-12-26',
                            'Stefanitag');
+        $this->_addTranslationForHoliday('boxingDay', 'en_EN', 'Boxing Day');
 
         /**
          * New Year´s Eve
@@ -297,6 +328,7 @@ class Date_Holidays_Driver_Austria extends Date_Holidays_Driver
         $this->_addHoliday('newYearsEve',
                            $this->_year . '-12-31',
                            'Silvester');
+        $this->_addTranslationForHoliday('newYearsEve', 'en_EN', 'New Years Eve');
 
         if (Date_Holidays::errorsOccurred()) {
             return Date_Holidays::getErrorStack();
